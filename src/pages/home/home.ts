6c36839beb3209167650e4446aae1e950b37ddcd
Álvaro Bunster <al.bunstr@gmail.com>
2018-10-16 02:26:32 -0300

    Primera parte de la app
    Se ha implementado hasta la reproducción de sonidos (clic).

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,48 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { ANIMALES } from '../../data/data.animales'
+import {AnimalInterface} from "../../interfaces/animal.interface";
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
+  animales: AnimalInterface[] = [];
+  audio = new Audio();
+  audioTime: any;
 
-  constructor(public navCtrl: NavController) {
-
+  constructor() {
+    // Copiar el objeto con splice
+    this.animales = ANIMALES.splice(0);
   }
 
+  reproducir(animal: AnimalInterface) {
+    this.pausarAudio(animal);
+
+    if (animal.reproduciendo) {
+      animal.reproduciendo = false;
+      return;
+    }
+
+    console.log(animal);
+
+    this.audio.src = animal.audio;
+    this.audio.load();
+    this.audio.play();
+    animal.reproduciendo = true;
+    this.audioTime = setTimeout(() => animal.reproduciendo = false, animal.duracion * 1000);
+  }
+
+  private pausarAudio(animalSel: AnimalInterface) {
+    clearTimeout(this.audioTime);
+    this.audio.pause();
+    this.audio.currentTime = 0;
+
+    for (let animal of this.animales) {
+      if (animal.nombre != animalSel.nombre) {
+        animal.reproduciendo = false;
+      }
+    }
+  }
 }
